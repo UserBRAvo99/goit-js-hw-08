cec9022db3c0bef64cb9f47383426f6441096e01
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-27 15:13:44 +0000

    Deploying to gh-pages from @ UserBRAvo99/goit-js-hw-08@9f7fe8e705473778428c7de9138e2fbce08fbf19 🚀

--- 03-feedback.3ed1e3e8.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");let t={};e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))})),e.addEventListener("submit",(function(a){a.preventDefault(),console.log(t),t={},localStorage.removeItem("feedback-form-state"),e.reset()})),function(){const a=localStorage.getItem("feedback-form-state");if(a)try{t=JSON.parse(a),Object.entries(t).forEach((([t,a])=>{e[t].value=a}))}catch(e){}}();
-//# sourceMappingURL=03-feedback.3ed1e3e8.js.map
